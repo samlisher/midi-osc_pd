e972b4ede073d9975cb6efb0151080cb46effde5
samlisher <64691717+samlisher@users.noreply.github.com>
2020-05-02 14:02:58 +0100

    Add files via upload
    Edited the startup script

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This script will start Pd in nogui
 # mode and will open the patch
-# named "test.pd"
+# named "MIDI_TO_OSC_PD.pd"
 echo "Starting Pd..."
-pd -nogui  -alsamidi  /home/pi/MIDI_TO_OSC_PD.pd & disown
+pd -nogui  -alsamidi  /home/pi/midi-osc_pd/MIDI_TO_OSC_PD.pd & disown
 
 sleep 5
 aconnect -x
