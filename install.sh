5ba560c07ceb3b7b7c26b74642b0cc801b11939f
samlisher <64691717+samlisher@users.noreply.github.com>
2020-05-02 15:33:00 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # This script will move the autostart into the correct folder
+sudo apt-get install puredata --yes && sudo apt-get install alsa-base alsa-utils --yes && sudo apt-get install aconnectgui --yes
 echo "copying..."
 sudo mv /home/pi/midi-osc_pd/autostart /etc/xdg/lxsession/LXDE-pi/
