cfe158b1629cc5011220f13184da84a1e4e601c3
samlisher <64691717+samlisher@users.noreply.github.com>
2020-05-03 18:07:13 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@
 # mode and will open the patch
 # named "MIDI_TO_OSC_PD.pd"
 echo "Starting Pd..."
-pd -nogui  -alsamidi  /home/pi/midi-osc_pd/MIDI_TO_OSC_PD.pd & disown
+pd -nogui -alsamidi -mididev 1  /home/pi/midi-osc_pd/MIDI_TO_OSC_PD.pd & disown
 
 sleep 5
 aconnect -x
