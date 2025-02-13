d437aeb23626ea48a3860b3fadb875932b89ef21
samlisher <64691717+samlisher@users.noreply.github.com>
2025-02-13 22:52:39 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # This script will move the autostart into the correct folder
-sudo apt-get install puredata --yes && sudo apt-get install alsa-base alsa-utils --yes && sudo apt-get install aconnectgui --yes
+sudo apt-get install puredata --yes && sudo apt install libasound2-dev --yes && sudo apt-get install aconnectgui --yes
+
+sleep 2
+
+sudo chmod +x /home/pi/midi-osc_pd/startup.sh
+
+sudo mv /home/pi/midi-osc_pd/startup.sh /home/pi
+
 echo "copying the autostart to initalize on boot..."
+
 sudo mv /home/pi/midi-osc_pd/autostart /etc/xdg/lxsession/LXDE-pi/
